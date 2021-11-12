c371aa5d10c77f98ef93bf2fb9a8d148f5b6c947
ytwotap <39551671+ytwotap@users.noreply.github.com>
2021-11-12 15:31:52 +0800

    race condition test

---
+++ java_concurrency/src/main/java/com/yt/capter/first/introduce/capter/ThreadSafeQuestion.java
@@ -0,0 +1,58 @@
+package com.yt.capter.first.introduce.capter;
+
+import com.yt.capter.tool.ThreadSafe;
+import com.yt.capter.tool.UnThreadSafe;
+import org.junit.Test;
+
+/**
+ * TODO
+ * race condition question
+ * 竞态条件问题
+ * @author: 杨涛
+ * @date: 2021/11/12/012
+ */
+public class ThreadSafeQuestion {
+    private  int value=10;
+    @UnThreadSafe
+    public int getNext() {
+        System.out.println(++this.value);
+        return this.value;
+    }
+
+
+    @ThreadSafe
+    public synchronized int getNext2() {
+        System.out.println(++this.value);
+        return this.value;
+    }
+
+    /**
+     * test getNext() refrence race conditon question
+     */
+    @Test
+    public void testGetNext() {
+        //多线程条件
+        //分别调用getNext()
+        Thread thread = new Thread(() -> getNext());
+        Thread thread2 = new Thread(() -> getNext());
+
+        //启动多线程
+        thread.start();
+        thread2.start();
+    }
+
+    /**
+     * test getNext2() not refrence race conditon question
+     */
+    @Test
+    public void testGetNext2() {
+        //多线程条件
+        //分别调用getNext()
+        Thread thread = new Thread(() -> getNext2());
+        Thread thread2 = new Thread(() -> getNext2());
+
+        //启动多线程
+        thread.start();
+        thread2.start();
+    }
+}
---
+++ java_concurrency/src/main/java/com/yt/capter/tool/ThreadSafe.java
@@ -0,0 +1,10 @@
+package com.yt.capter.tool;
+
+/**
+ * TODO
+ *线程安全的注解 只是标记作用
+ * @author: 杨涛
+ * @date: 2021/11/12/012
+ */
+public @interface ThreadSafe {
+}
